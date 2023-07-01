fbd01fd0826c0b1ed2c764a6382e05ada0f736c5
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-07-01 01:36:28 +0100

    Add the Typechecker

--- prototype/app/Syntax.hs
+++ prototype/app/Syntax.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable,
+   DeriveTraversable, TemplateHaskell, TypeFamilies #-}
 module Syntax where
 
+import Data.Functor.Foldable.TH (makeBaseFunctor)
 import Data.Map (Map)
--- import qualified Data.Map as M
+import qualified Data.Map as M
 -- import Data.IntMap (IntMap)
 -- import qualified Data.IntMap as IM
 
@@ -27,18 +30,28 @@ types, instead of by type synonyms
 * We don't account for GADTs.
 
 * We need to validate the datatype Name + Mults against the Name definition and the Mults it is actually expecting
+
+* We must also implement the algorithm for inferring usage environments, which
+we still need to figure out when exactly it should be run
 -}
 
 type Name = String
 
 data Id = MkId { varType   :: Ty
                , idBinding :: IdBinding
-               , varName   :: Name
+               , varName'   :: Name
                }
-newtype MultVar = MkMultVar { varName :: Name }
+               deriving (Eq, Show)
+newtype MultVar = MkMultVar { varName' :: Name }
+  deriving (Eq,Show)
+
+varName :: Var -> Name
+varName (Id _ _ n)  = n
+varName (MultVar n) = n
 
 data Var = Id' Id
          | MultVar' MultVar
+         deriving (Eq, Show)
 {-# COMPLETE Id, MultVar #-}
 
 pattern Id :: Ty -> IdBinding -> Name -> Var
@@ -50,6 +63,8 @@ pattern MultVar name = MultVar' (MkMultVar name)
 data Mult = Many       -- ω
           | One        -- 1
           | MV' MultVar -- π
+          deriving (Eq,Show)
+{-# COMPLETE Many,One,MV #-}
 
 pattern MV :: Name -> Mult
 pattern MV name = MV' (MkMultVar name)
@@ -57,33 +72,39 @@ pattern MV name = MV' (MkMultVar name)
 type UsageEnv = Map Name Mult
 
 data IdBinding = LambdaBound Mult
-               | LetBound UsageEnv
-               | CaseBound [UsageEnv] -- One UE per alternative
+               | DeltaBound UsageEnv -- ^ Let and case bound variables. Case variables do have many usage environments, but in practice (when they occur in a context), they have just one usage environment
+               deriving (Eq, Show)
 
 data Ty
+  -- = TyMultVar Name
   = Datatype Name [Mult]    -- K π_1 ... π_n
   | FunTy   Ty   Mult Ty    -- φ ->π σ
   | Scheme  Name Ty         -- ∀p. φ
+  deriving (Eq,Show)
 
-data Expr
-  = Var Var
-  | Lam Var Expr
-  | App Expr Expr
-  | Let Bind Expr
-  | Case Expr Var [Alt]
-  | Ty Ty
+data Expr b
+  = Var b -- Only term variables, type variables at the term level would be (Ty (TyMultVar "p"))
+  | Lam b (Expr b)
+  | App (Expr b) (Expr b)
+  | Let (Bind b) (Expr b)
+  | Case (Expr b) b [Alt b]
+  | Mult Mult
+  deriving (Eq,Show)
 
-data Bind = NonRec Var Expr
-          | Rec [(Var, Expr)]
+data Bind b = NonRec b (Expr b)
+            | Rec [(b, Expr b)]
+            deriving (Eq,Show)
 
-data Alt
-    = Alt AltCon [Var] Expr
+data Alt b
+    = Alt AltCon [b] (Expr b)
+    deriving (Eq,Show)
 
 data AltCon
   = DataAlt DataCon   --  ^ A plain data constructor: @case e of { Foo x -> ... }@.
                       -- Invariant: the 'DataCon' is always from a @data@ type, and never from a @newtype@
 
   | DEFAULT           -- ^ Trivial alternative: @case e of { _ -> ... }@
+  deriving (Eq,Show)
 
 -- | All constructors are of the form K~\overline{x{:}_\pi\sigma}
 -- We record the information about the multiplicity and type of each argument
@@ -94,11 +115,13 @@ data DataCon
             , dcUnivMultVars :: [Mult]
             , dcArgTys :: [Scaled Ty]
             }
+            deriving (Eq,Show)
 
 data Scaled a = Scaled !Mult a
+  deriving (Eq,Show)
 
-type Ctx = [Var]
--- ^ The context is a list of variables that can be
+type Ctx = Map Name Var
+-- ^ The context maps names to variables that can be
 -- * Lambda bound vars (x :_π σ)
 -- * Let bound vars    (x :_Δ σ)
 -- * Case bound vars   (x :_\ov{Δ} σ)
@@ -106,20 +129,40 @@ type Ctx = [Var]
 -- * DataCons          (K:σ), which are really just a special sort of top-level closed-Δ let-bound vars (K :_\ov{\cdot} σ)
 
 -- | Γ |- e : τ
-data Term = Term Ctx Expr Ty
+data Term b = Term Ctx (Expr b) Ty
+  deriving Show
 
+-- Recursion schemes
+makeBaseFunctor ''Expr
+makeBaseFunctor ''Ty
+
+type CoreTerm = Term Var
+type CoreAlt  = Alt Var
+type CoreBndr = Bind Var
+type CoreExpr = Expr Var
+
+erase :: CoreTerm -> CoreExpr
+erase (Term _ e _) = e
 
 -- Let's try writing a simple program like id @A
 -- id :: ∀ p. (A p) ->p (A p)
 -- id = /\p. \x:_pA -> x:A
-idProg :: Term
+idProg :: CoreTerm
 idProg
   = Term
-    [Id (Scheme "kp" (Datatype "A" [MV "p"])) (LetBound mempty) "MkA"]
+    (M.singleton "MkA" (Id (Scheme "kp" (Datatype "A" [MV "p"])) (DeltaBound mempty) "MkA"))
     (Lam (MultVar "p") $
        Lam (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x") $
          Var (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x")
     )
     (Scheme "p" (FunTy (Datatype "A" [MV "p"]) (MV "p") (Datatype "A" [MV "p"])))
 
+id2 :: CoreExpr
+id2 =
+ Lam (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x") $
+   Var (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x")
 
+idBad :: CoreExpr
+idBad =
+ Lam (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x") $
+   Var (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "y")
--- prototype/app/Typechecker.hs
+++ prototype/app/Typechecker.hs
@@ -1,1 +1,158 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 module Typechecker where
+
+import Data.Maybe
+import qualified Data.Map as M
+import Data.Functor.Foldable
+import Control.Monad.State
+import Control.Monad.Except
+import Control.Monad
+import Syntax
+import Control.Applicative (empty)
+
+{-
+Notes:
+
+* For now we don't do good error reporting
+* We use Input/Output context to track linear variables without context splitting
+* I'm missing the rule for Var_p
+
+* We don't allow multiplicity variables to appear anywhere besides function
+types, so we don't need a type-level multiplicity variable
+-}
+
+type Check = StateT Ctx Maybe
+
+runClosedCheck :: Check a -> Maybe a
+runClosedCheck check = do
+  (a,ctx) <- runStateT check mempty
+  when (not $ null $ M.elems $ M.filter (\case Id _ (LambdaBound One) _ -> True; _ -> False) ctx) (fail "Linear variables escaped")
+  return a
+
+projectLinear :: UsageEnv -> [Name]
+projectLinear = M.keys . M.filter (== One)
+
+-- | Use a variable from the environment.
+-- If it is a linearly-bound variable it will be consumed (and no longer available in the context)
+use :: Name -> Check Var
+use name = do
+  Just var <- gets (M.lookup name)
+  case var of
+    Id' var ->
+      case idBinding var of
+        LambdaBound Many -> pure ()
+        LambdaBound One -> do
+          modify (M.delete name)
+        LambdaBound (MV' _) -> do
+          -- ROMES:TODO: I suppose we must treat polymorphic variables as if
+          -- they were linear, bc they might be instantiated to 'One' later on.
+          modify (M.delete name)
+        DeltaBound ue -> do
+          _ <- mapM use (projectLinear ue) -- all elements should already have mult == One.
+          pure ()
+    MultVar' _ ->
+      pure ()
+  return var
+
+extend :: Var -> Check a -> Check a
+extend var check = do
+  modify (M.insert (varName var) var)
+  a <- check
+  case var of
+    MultVar _ -> return a
+    Id _ bnd name -> case bnd of
+      LambdaBound One -> do
+        True <- wasConsumed name
+        return a
+      LambdaBound (MV' _) -> do
+        True <- wasConsumed name
+        return a
+      LambdaBound Many -> return a
+      DeltaBound ue -> do
+        True <- all id <$> mapM wasConsumed (projectLinear ue)
+        return a
+  where
+    wasConsumed x = isNothing <$> gets (M.lookup x)
+
+-- | @substTy π n σ@ performs the substitution σ[π/n]
+substTy :: Mult -> Name -> Ty -> Ty
+substTy m name = \case
+  -- TyMultVar n' -> if n' == name then Mult m else TyMultVar n'
+  Datatype n' mults -> Datatype n' (map (substMult m name) mults)
+  FunTy   t1  m' t2 -> FunTy (substTy m name t1) (substMult m name m') (substTy m name t2)
+  Scheme  n' t1 -> if n' == name then Scheme n' t1 else Scheme n' (substTy m name t1)
+
+substMult :: Mult -> Name -> Mult -> Mult
+substMult m name = \case
+  One -> One
+  Many -> Many
+  MV n' -> if n' == name then m else MV n'
+
+-- | Asserts the input and output environments of a type-checking computation are the same
+-- i.e. no linear variables were consumed
+usesNoLinearVariables :: Check a -> Check a
+usesNoLinearVariables check = do
+  prev <- get
+  a <- check
+  after <- get
+  when (prev /= after) $
+    fail "Linear variables were consumed more than once (likely an unrestricted function was applied to an argument that uses linear variables, thereby consuming them unrestrictedly)"
+  return a
+
+-- Lam (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x") $
+--   Var (Id (Datatype "A" [MV "p"]) (LambdaBound (MV "p")) "x")
+typecheck :: CoreExpr -> Check (CoreExpr, Ty)
+typecheck = cata \case
+  VarF (MultVar _) -> error "impossible"
+  VarF (Id _ _ name) -> do
+    Id' id <- use name
+    -- TODO: Perhaps assert var matches one found in environment?
+    return (Var (Id' id), varType id)
+  LamF var e -> do
+    (e, t) <- extend var e
+    case var of
+      MultVar n -> return (Lam var e, Scheme n t)
+      Id _ (DeltaBound _) _ -> error "impossible"
+      Id vt (LambdaBound m) _ ->
+        return (Lam var e, FunTy vt m t)
+  AppF e1 e2       -> do
+    (e1,t1) <- e1
+    case t1 of
+      Scheme n schemet -> do
+        (Mult m,Datatype "Mult" []) <- e2 -- built-in "Mult" kind to "kindcheck" multiplicities
+        return (App e1 (Mult m), substTy m n schemet)
+      FunTy argt m rett -> do
+        case m of
+          One  -> do
+            (e2,t2) <- e2
+            checkEqTy argt t2
+            return (App e1 e2, rett)
+          MV _ -> do
+            -- just as above
+            (e2,t2) <- e2
+            checkEqTy argt t2
+            return (App e1 e2, rett)
+          Many -> do
+            (e2,t2) <- usesNoLinearVariables e2
+            checkEqTy argt t2
+            return (App e1 e2, rett)
+      _ -> empty
+  LetF bind _      -> undefined
+  CaseF _ var alts -> undefined
+  MultF One  -> return (Mult One, Datatype "Mult" [])
+  MultF Many -> return (Mult One, Datatype "Mult" [])
+  MultF (MV m) -> do
+    Just _ <- gets (M.lookup m)
+    return (Mult (MV m), Datatype "Mult" [])
+
+exprTy :: CoreExpr -> Ty
+exprTy = cata undefined
+
+
+checkEqTy :: Ty -> Ty -> Check ()
+checkEqTy t1 t2 = if t1 == t2 then pure () else fail $ "Expecting " ++ show t1 ++ " and " ++ show t2 ++ " to match."
+
