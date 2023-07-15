fc25fe0e3cc0956e0372d2a5e5b37de7a6dc4345
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-07-15 22:36:23 +0100

    Realize the fragmented usage environment idea does not work trivially
    It's not easy at all to think about the semantic linearity in the lazy
    language, but it looks like I'm able to prove that linear double free is
    well-typed, which is awful.
    
    In restrospect, the branch cases cannot have the things that are used in
    the scrutinee at all, and likely need to consume the bound things too.
    
    We just need to realise why exactly those intermediate programs in the
    reverse binder swap preserve linearity sematnically, and then how we
    can make them well-typed

--- laboratory/app/Files.hs
+++ laboratory/app/Files.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE QualifiedDo #-}
 module Files where
 
 import Prelude.Linear
-
--- import Prelude hiding (Functor, Applicative, Monad)
-
+import qualified Control.Functor.Linear as Control
+import qualified System.IO.Resource.Linear as Linear
+import qualified Data.Text as Text
+import qualified Prelude
 
 -- class Functor f where
 --   fmap :: (a %1 -> b) -> f a %1 -> f b
@@ -17,4 +19,9 @@ import Prelude.Linear
 -- class Applicative m => Monad m where
 --   (>>=) :: m a %1 -> (a %1 -> m b) %1 -> m b
 
-
+linearWriteToFile :: IO ()
+linearWriteToFile = Linear.run $ Control.do
+  handle1 <- Linear.openFile "test.txt" Linear.WriteMode
+  handle2 <- Linear.hPutStrLn handle1 (Text.pack "hello there")
+  () <- Linear.hClose handle2
+  Control.return (Ur ())
--- laboratory/app/Main.hs
+++ laboratory/app/Main.hs
@@ -6,18 +6,35 @@
 {-# LANGUAGE RebindableSyntax #-}
 module Main where
 
+import qualified Prelude
 import GHC.Exts (Multiplicity(..))
 
 import Prelude.Linear
+import Data.Unrestricted.Linear
 import qualified System.IO.Linear as Linear
 import System.IO
 import Control.Functor.Linear as Linear
+import Prelude.Linear
+import qualified Control.Functor.Linear as Control
+import qualified System.IO.Resource.Linear as Linear
+import qualified Data.Text as Text
+import qualified Prelude
+
+import Files
 
 -- f1 :: a %1 -> (a, a)
 -- f1 x = (x,x)
 
 
-
 f2 :: a %Many -> (a, a)
 f2 x = (x,x)
 
+main :: IO ()
+main = Files.linearWriteToFile
+
+linearWriteToFile :: IO ()
+linearWriteToFile = Linear.run $ Control.do
+  handle1 <- Linear.openFile "test.txt" Linear.WriteMode
+  handle2 <- Linear.hPutStrLn handle1 (Text.pack "hello there")
+  () <- Linear.hClose handle2
+  Control.return (Ur ())
